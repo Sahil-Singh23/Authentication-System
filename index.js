d10daca96cb3eab347c4dd448bc6898663734471
Sahil-Singh23 <sahil091205@gmail.com>
2025-08-15 22:58:00 +0530

    fixed bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 require("dotenv").config();
-const jwtSecret = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 let users = [];
 
